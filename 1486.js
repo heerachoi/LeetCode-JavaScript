103a0a334a0627cee4416b8cbc3c53650730538e
heerachoi <heera7722@gmail.com>
2021-05-19 14:32:43 +0900

    Update 1486.js

--- 1486.js
+++ 1486.js
@@ -1,4 +1,13 @@
 // #1486. XOR Operation in an Array
+// Given an integer n and an integer start.
+// Define an array nums where nums[i] = start + 2*i (0-indexed) and n == nums.length.
+// Return the bitwise XOR of all elements of nums.
+
+// Ex.
+// Input: n = 5, start = 0
+// Output: 8
+// Explanation: Array nums is equal to [0, 2, 4, 6, 8] where (0 ^ 2 ^ 4 ^ 6 ^ 8) = 8.
+
 // 1. save the numbers in an array
 // 2. operate xor
 
