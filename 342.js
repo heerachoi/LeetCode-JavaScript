7335ab4d24d81fefdbfd7078753305d00212f10d
heerachoi <heera7722@gmail.com>
2021-05-22 15:33:46 +0900

    Power of Four

---
+++ 342.js
@@ -0,0 +1,28 @@
+// #342 Power of Four
+// 문제
+// Given an integer n, return true if it is a power of four. Otherwise, return false.
+// An integer n is a power of four, if there exists an integer x such that n == 4x.
+
+// Ex 1
+// Input: n = 16
+// Output: true
+
+// Ex 2
+// Input: n = 1
+// Output: true
+
+var isPowerOfFour = function(n) {
+    if(n == 0) {
+        return false;
+    }
+    while(n != 1) {   
+        if(n % 4 != 0) {
+            return false;
+        }
+        n = n / 4;   
+    }
+    return true;
+};
+
+let n = 8;
+console.log(isPowerOfFour(n));
