5eeaaefc80511f46252aa57e219c00058708c7a5
heerachoi <heera7722@gmail.com>
2021-05-26 18:31:58 +0900

    Delete Node in a Linked List

---
+++ 237.js
@@ -0,0 +1,27 @@
+// 237. Delete Node in a Linked List
+
+// Write a function to delete a node in a singly-linked list. You will not be given access to the head of the list, instead you will be given access to the node to be deleted directly.
+// It is guaranteed that the node to be deleted is not a tail node in the list.
+
+// Example1
+// Input: head = [4,5,1,9], node = 5
+// Output: [4,1,9]
+// Explanation: You are given the second node with value 5, the linked list should become 4 -> 1 -> 9 after calling your function.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+ var deleteNode = function(node) {
+    //set node.next.next
+    node.val = node.next.val;  //set current node vlaue to next node value
+    node.next = node.next.next; //move the node.next pointer to node of next.next to skip the deleteNode.
+    console.log("node = " + node);
+};
