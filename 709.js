a9c19e2e3d732cc598e4027b4c519d2e1d45bbaa
heerachoi <heera7722@gmail.com>
2021-05-28 23:32:18 +0900

    To Lower Case

---
+++ 709.js
@@ -0,0 +1,20 @@
+// 709. To Lower Case
+
+// Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+// Example 1:
+// Input: s = "Hello"
+// Output: "hello"
+
+// Example 2:
+// Input: s = "here"
+// Output: "here"
+
+/**
+ * @param {string} str
+ * @return {string}
+ */
+ var toLowerCase = function(str) {
+    return str.toLowerCase();
+   
+};
